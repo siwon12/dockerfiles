81f454074e343004093c48b706e1e2118835455b
Young Joon Lee <entelecheia@hotmail.com>
2023-05-08 20:14:57 +0900

    refactor(webui): comment out venv creation and activation code

--- a1111/.docker/scripts/webui.sh
+++ a1111/.docker/scripts/webui.sh
@@ -196,21 +196,21 @@ printf "\n%s\n" "${delimiter}"
     exit 1
 }
 
-printf "\n%s\n" "${delimiter}"
-printf "Create and activate python venv"
-printf "\n%s\n" "${delimiter}"
-if [[ ! -d "${venv_dir}" ]]; then
-    "${python_cmd}" -m venv "${venv_dir}"
-fi
-# shellcheck source=/dev/null
-if [[ -f "${venv_dir}"/bin/activate ]]; then
-    source "${venv_dir}"/bin/activate
-else
-    printf "\n%s\n" "${delimiter}"
-    printf "\e[1m\e[31mERROR: Cannot activate python venv, aborting...\e[0m"
-    printf "\n%s\n" "${delimiter}"
-    exit 1
-fi
+# printf "\n%s\n" "${delimiter}"
+# printf "Create and activate python venv"
+# printf "\n%s\n" "${delimiter}"
+# if [[ ! -d "${venv_dir}" ]]; then
+#     "${python_cmd}" -m venv "${venv_dir}"
+# fi
+# # shellcheck source=/dev/null
+# if [[ -f "${venv_dir}"/bin/activate ]]; then
+#     source "${venv_dir}"/bin/activate
+# else
+#     printf "\n%s\n" "${delimiter}"
+#     printf "\e[1m\e[31mERROR: Cannot activate python venv, aborting...\e[0m"
+#     printf "\n%s\n" "${delimiter}"
+#     exit 1
+# fi
 
 # Try using TCMalloc on Linux
 prepare_tcmalloc() {
